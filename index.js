1322eeb1face3ab63cc67887cd298a1c0677a83b
Kenny Arehart <kenny.arehart@ff0000.com>
2018-05-18 11:53:59 -0700

    Changed index.js to be export point for sub modules methods, leading to single output module

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import * as Velvet from './lib/Velvet'
+import * as DataManager from './lib/DataManager'
 import * as Capture from './lib/Capture'
-export { Capture, Velvet }
+import * as EventController from './lib/EventController.js'
+import * as VelvetEvents from './lib/VelvetEvents.js'
+import G from './lib/get.js'
+
+export const init = DataManager.init
+export const isPreviewLocation = DataManager.isPreviewLocation
+export const addEventListener = EventController.add
+export const removeEventListener = EventController.remove
+export const events = VelvetEvents
+export const capture = Capture
+export const get = G
