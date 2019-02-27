511ddd1c56ab15f1e24290e81a12b3374982b665
Kenny Arehart <kenny.arehart@kennya-mbp-2.ff0000.com>
2019-02-26 16:19:43 -0800

    Added convertBreak() method to new format file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import * as C from './lib/capture.js'
 import * as EC from './lib/event-controller.js'
 import * as E from './lib/events.js'
 import G from './lib/get.js'
+import * as F from './lib/format.js'
 
 export const init = DM.init
 export const isPreviewLocation = DM.isPreviewLocation
@@ -11,3 +12,4 @@ export const removeEventListener = EC.remove
 export const events = E
 export const capture = C
 export const get = G
+export const convertBreaks = F.convertBreaks
